7578fc0b78ad8567b83fae8438972ba00c42b93f
sanaedesuyo <1311224311@qq.com>
2025-01-03 21:39:09 +0800

    added expression class.

---
+++ expression/expr.cpp
@@ -0,0 +1,34 @@
+﻿#include "expr.h"
+#include <stdexcept>
+
+expr::~expr() = default;
+
+double number::explain() {
+    return val_;
+}
+
+symbol::~symbol() {
+    delete l_;
+    delete r_;
+}
+
+double add::explain() {
+    return l_->explain() + r_->explain();
+}
+
+double minus::explain() {
+    return l_->explain() - r_->explain();
+}
+
+double mul::explain() {
+    return l_->explain() * r_->explain();
+}
+
+double div::explain() {
+    const double r_val = r_->explain();
+    if (r_val == 0.0) {
+        throw std::runtime_error("Division by zero");
+    }
+
+    return l_->explain() / r_val;
+}
---
+++ expression/expr.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+class expr {
+public:
+    virtual ~expr() = 0;
+    virtual double explain() = 0;
+};
+
+class number final : public expr {
+protected:
+    double val_;
+public:
+    explicit number(const double val) : val_(val) {}
+    double explain() override;
+
+    ~number() override = default;
+};
+
+class symbol : public expr {
+protected:
+    expr* l_;
+    expr* r_;
+public:
+    symbol(expr* l, expr* r)
+        : l_(l),
+          r_(r) {}
+    ~symbol() override;
+};
+
+class add final : public symbol {
+public:
+    add(expr* l, expr* r)
+        : symbol(l, r) {
+    }
+
+    double explain() override;
+};
+
+class minus final : public symbol {
+public:
+    minus(expr* l, expr* r)
+        : symbol(l, r) {
+    }
+
+    double explain() override;
+};
+
+class mul final : public symbol {
+public:
+    mul(expr* l, expr* r)
+        : symbol(l, r) {
+    }
+
+    double explain() override;
+};
+
+class div final : public symbol {
+public:
+    div(expr* l, expr* r)
+        : symbol(l, r) {
+    }
+
+    double explain() override;
+};
